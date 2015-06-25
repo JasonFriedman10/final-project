944192ab0295e4a1210b19a2cc455a4184c03412
Griffin Perry <c18gp@dalton.org>
2015-06-25 14:54:19 +0000

    Did not add gemfile :)

--- application_controller.rb
+++ application_controller.rb
@@ -12,18 +12,9 @@ class MyApp < Sinatra::Base
     erb :form
   end
   
-  post "/form" do 
-<<<<<<< HEAD
-#      @form = Form.new(params["name"], params["position"], params["pace"], params["shooting"], params["passing"], params["dribbling"], params["defending"], params["physicality"], params["card"], params["image"], params["club"], para      ms["flag"])
-=======
-<<<<<<< HEAD
-     @form = Form.new(params["name"], params["position"], params["pace"], params["shooting"], params["passing"], params["dribbling"], params["defending"], params["physicality"], params["card"], params["image"], params["club"], params["flag"])
-     erb :form
-=======
-#      @form = Form.new(params["name"], params["position"], params["pace"], params["shooting"], params["passing"], param      s["dribbling"], params["defending"], params["physicality"], params["card"], params["image"], params["club"], para      ms["flag"])
->>>>>>> ea0ac89f3443b750914c36f2ed08b7aa52607073
+#   post "/form" do 
+#      @form = Form.new(params["name"], params["position"], params["pace"], params["shooting"], params["passing"], params["dribbling"], params["defending"], params["physicality"], params["card"], params["image"], params["club"], params["flag"])
 #      erb :form
->>>>>>> f645ed49c80ff3aff87094df281635e00f5945b5
-  end
+#   end
   
 end

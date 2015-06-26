40bc025e7a2997553795241ddad68c1171369444
Jason Friedman <jasonfriedman10@icloud.com>
2015-06-26 14:16:18 +0000

    added changes

--- application_controller.rb
+++ application_controller.rb
@@ -8,16 +8,14 @@ class MyApp < Sinatra::Base
     erb :index
   end
   
-  get "/form" do
+  get "/form" doff fd
     erb :form
   end
-  
-   
-  
+ 
   
    post '/card' do
       @form = Form.new(params["name"], params["position"], params["pace"], params["shooting"], params["passing"], params["dribbling"], params["defending"], params["physicality"], params["card"], params["club"], params["flag"], params["image"])
-#      @form.convert
+#       @form.convert
      erb :card
   end
   

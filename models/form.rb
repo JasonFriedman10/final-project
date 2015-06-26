adab7a31644ddc93675c8c15a227cddb85bbc20d
Jared Wasserman <jwass91@gmail.com>
2015-06-26 16:18:10 +0000

    added sessions that dont work

--- models/form.rb
+++ models/form.rb
@@ -17,9 +17,9 @@ class Form
   end
   
 # #     begin
-#    def convert
+   def convert
 #       create an API client instance
-#       client = Pdfcrowd::Client.new("jwass91", "97a22b938927db9094e3f2f45e0ae13b")
+      client = Pdfcrowd::Client.new("jwass91", "97a22b938927db9094e3f2f45e0ae13b")
 
 # # #     convert a web page and store the generated PDF into a pdf variable
 # #       pdf = client.convertURI('http://jwass91-pc-15-precollege-se1-nyda-061515-1-106835.nitrousapp.com:9393/card')
@@ -30,7 +30,7 @@ class Form
 #      File.open('fifa_card.pdf', 'wb') {|f| client.convertHtml(html, f)}
 
 # # #    convert an HTML file
-# # #     File.open('fifa_card.pdf', 'wb') {|f| client.convertFile('/home/nitrous/final-project/views/card.erb', f)}
+    File.open('fifa_card.pdf', 'wb') {|f| client.convertFile('http://jwass91-pc-15-precollege-se1-nyda-061515-1-106835.nitrousapp.com:9393/card', f)}
 
 # # #     retrieve the number of tokens in your account
 # # #     ntokens = client.numTokens()
@@ -39,7 +39,7 @@ class Form
 # # #         print 'FAILED: ', why
 # # #     end
     
-#    end
+     end
 end
 
 
